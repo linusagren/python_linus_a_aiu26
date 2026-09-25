24bfbf69d1635f9ae0fc381432f0c865c2f6b073
Linus <linus_agren@live.com>
2026-09-25 10:40:56 +0200

    finished e4, e5

--- exercises/Exercise 2/e4_luggage_size.py
+++ exercises/Exercise 2/e4_luggage_size.py
@@ -6,16 +6,18 @@ dimensions: 55x40x23cm (length x width x height)
 Let the user input weight, length, width and height of the luggage. The program should check if the luggage
 is allowed or not.
 """
+max_weight = 8
+max_dimensions = [55, 40, 23]
 
 luggage_weight = int(input("Enter luggage weight (KG): "))
-luggage_dimensions = [0, 0, 0]
+luggage_dimensions = ["length", "width", "height"]
 
-for i in range(len(luggage_dimensions)):
-    match i:
-        case 0:
-            luggage_dimensions[i] = int(input("Enter luggage length: "))
-        case 1:
-            luggage_dimensions[i] = int(input("Enter luggage width: "))
-        case 2:
-            luggage_dimensions[i] = int(input("Enter luggage height: "))
+if luggage_weight <= 8:
+    for i in range(len(luggage_dimensions)):
+        luggage_dimensions[i] = int(input(f"Enter luggage {luggage_dimensions[i]}: "))
 
+        if luggage_dimensions[i] > max_dimensions[i]:
+            print("Luggage is too large.")
+            break
+else:
+    print("Luggage is too heavy.")
